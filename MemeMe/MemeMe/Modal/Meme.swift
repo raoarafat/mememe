497a90b1baaac7460f19d5957f04ac7174f5cb8e
raoarafat <rak_4yours@hotmail.com>
2015-05-02 01:29:27 +0800

    Committed by Udacity

---
+++ MemeMe/MemeMe/Modal/Meme.swift
@@ -0,0 +1,19 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Arafat on 4/28/15.
+//  Copyright (c) 2015 Arafat. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    
+    var toptxt   : String!
+    var bottomtxt: String!
+    var image    : UIImage!
+    var memeImage: UIImage!
+
+}
+
